f84be1d2ddf36f4c8ca91f9c41ed89b12cacc7cb
TatShen <shenets-97@mail.ru>
2023-07-17 07:54:31 +0300

    add swiper

--- src/Components/molecules/Team/Team.jsx
+++ src/Components/molecules/Team/Team.jsx
@@ -1,55 +1,124 @@
-import Card from '../../atoms/Card/Card'
-import Text from '../../atoms/Text/text'
-import './team.scss'
-import React from 'react'
+import Card from "../../atoms/Card/Card";
+import Text from "../../atoms/Text/text";
+import "./team.scss";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
+class Team extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth,
+    };
+  }
 
+  update = () => {
+    this.setState({ windowWidth: window.innerWidth });
+  };
 
-class Team extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          windowWidth: window.innerWidth,
-        };
-      }
-    
-      update=()=>{
-        this.setState({windowWidth:window.innerWidth})
-      }
-    
-      componentDidMount(){
-        window.addEventListener('resize', this.update)
-      }
-    
-      componentWillUnmount(){
-        window.removeEventListener('resize', this.update)
-      }
-      
-    render(){
-        return(
-            <div className='team' id='team'>
-                <div className='content'>
-                    <Text className='text_h' value='НАША КОМАНДА'></Text>
-                    <div className='team_description'>
-                        <Card name='Яна Воронежская' role='Учредитель' img='images/team1.png'></Card>
-                        <Card name='Денис Дмитриев' role='Креативный директор' img='images/team2.png'></Card>
-                        <div className='text'>
-                             <Text className='team_description_text' value='Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах'></Text>
-                             {(this.state.windowWidth<=895)? '':<Text className='team_description_text'value='Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах'></Text>} 
-                            
-                        </div>
-                        <div className='text'>
-                             <Text className='team_description_text' value='Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах'></Text>
-                             {(this.state.windowWidth<=895)? '':<Text className='team_description_text'value='Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах'></Text>} 
-                        </div>
-                        <Card name='Яна Воронежская' role='Учредитель' img='images/team1.png'></Card>
-                        <Card name='Денис Дмитриев' role='Креативный директор' img='images/team2.png'></Card>
-                        
-                    </div>
-                </div>
+  componentDidMount() {
+    window.addEventListener("resize", this.update);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.update);
+  }
+
+  render() {
+    return this.state.windowWidth >= 480 ? (
+      <div className="team" id="team">
+        <div className="content">
+          <Text className="text_h" value="НАША КОМАНДА"></Text>
+          <div className="team_description">
+            <Card
+              name="Яна Воронежская"
+              role="Учредитель"
+              img="images/team1.png"
+            ></Card>
+            <Card
+              name="Денис Дмитриев"
+              role="Креативный директор"
+              img="images/team2.png"
+            ></Card>
+            <div className="text">
+              <Text
+                className="team_description_text"
+                value="Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах"
+              ></Text>
+              {this.state.windowWidth <= 895 ? (
+                ""
+              ) : (
+                <Text
+                  className="team_description_text"
+                  value="Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах"
+                ></Text>
+              )}
             </div>
-        )
-    }
+            <div className="text">
+              <Text
+                className="team_description_text"
+                value="Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах"
+              ></Text>
+              {this.state.windowWidth <= 895 ? (
+                ""
+              ) : (
+                <Text
+                  className="team_description_text"
+                  value="Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах"
+                ></Text>
+              )}
+            </div>
+            <Card
+              name="Яна Воронежская"
+              role="Учредитель"
+              img="images/team1.png"
+            ></Card>
+            <Card
+              name="Денис Дмитриев"
+              role="Креативный директор"
+              img="images/team2.png"
+            ></Card>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div className="team" id="team">
+        <div className="content">
+          <Text className="text_h" value="НАША КОМАНДА"></Text>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <Card
+                name="Яна Воронежская"
+                role="Учредитель"
+                img="images/team1.png"
+              ></Card>
+            </SwiperSlide>
+            <SwiperSlide>
+            <Card
+              name="Денис Дмитриев"
+              role="Креативный директор"
+              img="images/team2.png"
+            ></Card>
+            </SwiperSlide>
+            <SwiperSlide>
+            <Text
+                  className="team_description_text"
+                  value="Дизайн конференция уже шестой год подряд объединяет профессионалов в сфере  цифровых технологий и инноваций, которые делятся своими идеями, полезной информацией о новых продуктах и сервисах"
+                ></Text>
+            </SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            ...
+          </Swiper>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Team
+export default Team;
